c3cb244a3b06f0ce70ad0c35f68ce30faf35003c
Arvind Saini <arvindsaini7536@gmail.com>
2025-10-10 07:43:14 +0530

    created path

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
 
-function App() {
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Pages/home'
+import About from './Pages/about'
+import Signin from './Pages/signin'
+import Singup from './Pages/Singup'
+import Profile from './Pages/Profile'
+
+export default function App() {
   return (
-    <div className='text-red-400'>
-      App
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-in' element={<Signin/>}/>
+        <Route path='/sign-up' element={<Singup/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
-
-export default App
